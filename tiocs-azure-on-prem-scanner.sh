98ea18a3f761fcc5c7075c05519054d24834b009
Adam Bradley <bradles76@hotmail.com>
2020-06-30 21:06:13 +1000

    Update tiocs-azure-on-prem-scanner.sh
    Changed variables

--- tiocs-azure-on-prem-scanner.sh
+++ tiocs-azure-on-prem-scanner.sh
@@ -3,12 +3,12 @@
 #This script expects the following environment variables to be set:
 # BUILD_BUILDID - This is set by Azure if you have called this script from a Docker Build and Push step
 # IMAGEREPOSITORY - This is set by Azure if you have called this script from a Docker Build and Push step
-TIOACCESSKEY=$1
-TIOSECRETKEY=$2
-TIOJFROGPASS=$3
+TIOACCESSKEYS=$1
+TIOSECRETKEYS=$2
+TIOJFROGPASSS=$3
 
 echo "Checking $IMAGEREPOSITORY:$BUILD_BUILDID and analyzing results on-premise then reporting into cloud.tenable.com repo $IMAGEREPOSITORY"
-echo "Tenable.io Access Key: $TIOACCESSKEY"
+echo "Tenable.io Access Key: $TIOACCESSKEYS"
 echo ""
 
 #For debugging
@@ -17,12 +17,12 @@ set
 
 echo "Download Tenable.io on-prem scanner"
 
-docker login --username pubread --password $TIOJFROGPASS tenableio-docker-consec-local.jfrog.io
+docker login --username pubread --password $TIOJFROGPASSS tenableio-docker-consec-local.jfrog.io
 docker pull tenableio-docker-consec-local.jfrog.io/cs-scanner:latest
 
 #For debugging
 docker images
 
 echo "Start of on-prem analysis"
-docker save $CONTAINERREGISTRY/$IMAGEREPOSITORY:$BUILD_BUILDID | docker run -e CHECK_POLICY=true -e DEBUG_MODE=true -e TENABLE_ACCESS_KEY=$TIOACCESSKEY -e TENABLE_SECRET_KEY=$TIOSECRETKEY -e IMPORT_REPO_NAME=$IMAGEREPOSITORY -i tenableio-docker-consec-local.jfrog.io/cs-scanner:latest inspect-image $IMAGEREPOSITORY:$BUILD_BUILDID
+docker save $CONTAINERREGISTRY/$IMAGEREPOSITORY:$BUILD_BUILDID | docker run -e CHECK_POLICY=true -e DEBUG_MODE=true -e TENABLE_ACCESS_KEY=$TIOACCESSKEYS -e TENABLE_SECRET_KEY=$TIOSECRETKEYS -e IMPORT_REPO_NAME=$IMAGEREPOSITORY -i tenableio-docker-consec-local.jfrog.io/cs-scanner:latest inspect-image $IMAGEREPOSITORY:$BUILD_BUILDID
 
